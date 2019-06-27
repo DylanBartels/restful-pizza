747a360763e4018a45e80329796f07beef44a159
Dylan Bartels <d_bartels@live.nl>
2019-06-27 14:24:39 +0200

    Add psql as database

--- mcb_lab/settings.py
+++ mcb_lab/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'mcb_lab.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mcb_lab',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 

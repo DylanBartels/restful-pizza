1f5eddf87a074e918d9b4b50c543ad8ba119c5b0
Dylan Bartels <d_bartels@live.nl>
2019-06-27 10:39:21 +0200

    Add basic models order and pizza

--- mcb_lab/pizza_ordering_service/models.py
+++ mcb_lab/pizza_ordering_service/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = ['ordered', 'preparing', 'delivering', 'delivered']
+PIZZA_FLAVOURS = ['margherita', 'pepperoni', 'mozzarella']
+PIZZA_SIZES = ['small', 'medium', 'large']
+
+class Order(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(choices=STATUS_CHOICES, default='ordered', max_length=100)
+
+    class Meta:
+        ordering = ('created',)
+
+class Pizza(models.Model):
+    flavor = models.MultipleChoiceField(required=True, choices=PIZZA_FLAVOURS)
+    size = models.MultipleChoiceField(required=True, choices=PIZZA_SIZES)
+    number = models.IntegerField(required=True, default=1)
+
+    class Meta:
+        ordering = ('created',)

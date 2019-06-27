0e6c79b7f7148f27e5abc1db61b60cf9e43ebc58
Dylan Bartels <d_bartels@live.nl>
2019-06-27 11:12:47 +0200

    Add basic views order and pizza

--- mcb_lab/pizza_ordering_service/views.py
+++ mcb_lab/pizza_ordering_service/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from pizza_ordering_service.models import Order, Pizza
+from pizza_ordering_service.serializers import OrderSerializer, PizzaSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class OrderList(generics.ListAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderDetail(generics.RetrieveAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class PizzaList(generics.ListAPIView):
+    queryset = Pizza.objects.all()
+    serializer_class = PizzaSerializer
+
+
+class PizzaDetail(generics.RetrieveAPIView):
+    queryset = Pizza.objects.all()
+    serializer_class = PizzaSerializer

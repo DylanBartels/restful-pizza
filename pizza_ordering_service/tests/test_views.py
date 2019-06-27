726d04b436503bf662568ae9084ed027e3a80704
Dylan Bartels <d_bartels@live.nl>
2019-06-27 16:31:26 +0200

    Add GetSinglePizzaTest

--- pizza_ordering_service/tests/test_views.py
+++ pizza_ordering_service/tests/test_views.py
@@ -24,3 +24,25 @@ class GetAllPizzasTest(TestCase):
         serializer = PizzaSerializer(pizzas, many=True)
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class GetSinglePizzaTest(TestCase):
+    """ Test module for GET single pizza API """
+
+    def setUp(self):
+        self.small_margherita = Pizza.objects.create(flavor='margherita', size='small')
+        self.medium_margherita = Pizza.objects.create(flavor='margherita', size='medium')
+        self.large_pepperoni = Pizza.objects.create(flavor='pepperoni', size='large')
+        self.small_pepperoni = Pizza.objects.create(flavor='pepperoni', size='small')
+
+    def test_get_valid_single_pizza(self):
+        response = client.get(
+            reverse('PizzaDetail', kwargs={'pk': self.large_pepperoni.pk}))
+        pizza = Pizza.objects.get(pk=self.large_pepperoni.pk)
+        serializer = PizzaSerializer(pizza)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_invalid_single_pizza(self):
+        response = client.get(
+            reverse('PizzaDetail', kwargs={'pk': 30}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)

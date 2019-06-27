7661d321e03417547da7f8f86884898838791546
Dylan Bartels <d_bartels@live.nl>
2019-06-27 16:25:51 +0200

    Add name to urls to refer to with testing

--- pizza_ordering_service/urls.py
+++ pizza_ordering_service/urls.py
@@ -3,10 +3,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from pizza_ordering_service import views
 
 urlpatterns = [
-    path('pizzas/', views.PizzaList.as_view()),
-    path('pizzas/<int:pk>/', views.PizzaDetail.as_view()),
-    path('orders/', views.OrderList.as_view()),
-    path('orders/<int:pk>/', views.OrderDetail.as_view()),
+    path('pizzas/', views.PizzaList.as_view(), name='PizzaList'),
+    path('pizzas/<int:pk>/', views.PizzaDetail.as_view(), name='PizzaDetail'),
+    path('orders/', views.OrderList.as_view(), name='OrderList'),
+    path('orders/<int:pk>/', views.OrderDetail.as_view(), name='OrderDetail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)

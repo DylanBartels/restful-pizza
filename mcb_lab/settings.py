9bf8e384cbbde878dc951388cd969559862efa68
Dylan Bartels <d_bartels@live.nl>
2019-06-27 12:48:40 +0200

    Add django restful settings

--- mcb_lab/settings.py
+++ mcb_lab/settings.py
@@ -121,3 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Django rest_framework
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [],
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
+}

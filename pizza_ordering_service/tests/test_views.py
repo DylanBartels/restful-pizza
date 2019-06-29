f72038cd8572ed00b67db786741db7df841f28e2
Dylan Bartels <d_bartels@live.nl>
2019-06-29 14:30:30 +0200

    Add tests for DELETE pizza

--- pizza_ordering_service/tests/test_views.py
+++ pizza_ordering_service/tests/test_views.py
@@ -34,7 +34,8 @@ class GetSinglePizzaTest(TestCase):
         Pizza.objects.create(flavor='margherita', size='small', order=Order.objects.get(status='creating'))
         Pizza.objects.create(flavor='margherita', size='medium', order=Order.objects.get(status='creating'))
         Pizza.objects.create(flavor='pepperoni', size='small', order=Order.objects.get(status='creating'))
-        self.large_pepperoni = Pizza.objects.create(flavor='pepperoni', size='large', order=Order.objects.get(status='creating'))
+        self.large_pepperoni = Pizza.objects.create(
+            flavor='pepperoni', size='large', order=Order.objects.get(status='creating'))
 
     def test_get_valid_single_pizza(self):
         response = client.get(
@@ -46,5 +47,57 @@ class GetSinglePizzaTest(TestCase):
 
     def test_get_invalid_single_pizza(self):
         response = client.get(
+            reverse('pizza-detail', kwargs={'pk': 30}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+class CreateNewPizzaTest(TestCase):
+    """ Test module for inserting a new pizza """
+
+    def setUp(self):
+        Order.objects.create(status='creating')
+        self.valid_payload = {
+            'flavor': 'margherita',
+            'size': 'small',
+            'order': Order.objects.get(status='creating')
+        }
+        self.invalid_payload = {
+            'flavor': 'pepperoni',
+            'size': 'large',
+            'order': ''
+        }
+
+    def test_create_valid_pizza(self):
+        response = client.post(
+            reverse('pizza-list'),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_invalid_pizza(self):
+        response = client.post(
+            reverse('pizza-list'),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+class DeleteSinglePizzaTest(TestCase):
+    """ Test module for deleting an existing pizza record """
+
+    def setUp(self):
+        Order.objects.create(status='creating')
+        self.large_pepperoni = Pizza.objects.create(
+            flavor='pepperoni', size='large', order=Order.objects.get(status='creating'))
+        self.medium_mozzarella = Pizza.objects.create(
+            flavor='mozzarella', size='medium', order=Order.objects.get(status='creating'))
+
+    def test_valid_delete_pizza(self):
+        response = client.delete(
+            reverse('pizza-detail', kwargs={'pk': self.large_pepperoni.pk}))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_invalid_delete_pizza(self):
+        response = client.delete(
             reverse('pizza-detail', kwargs={'pk': 30}))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)

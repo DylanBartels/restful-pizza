92fbcb33462515d47624127fea3cdf3d83efdb41
Dylan Bartels <d_bartels@live.nl>
2019-07-02 10:52:21 +0200

    Add actual createmigrate and migrate to script

--- scripts/clean_database.sh
+++ scripts/clean_database.sh
@@ -5,5 +5,6 @@ CREATE DATABASE mcb_lab;
 GRANT ALL PRIVILEGES ON DATABASE mcb_lab TO admin;
 ALTER USER admin CREATEDB;
 EOF
-
+python manage.py makemigrations pizza_ordering_service
+python manage.py migrate
 exit

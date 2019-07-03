22d8fa2c649dcebfcf6cfb3cf3c75ae3b01e541c
Dylan Bartels <d_bartels@live.nl>
2019-07-03 16:31:18 +0200

    Add custom create/update for Specification serializer

--- pizza_ordering_service/serializers.py
+++ pizza_ordering_service/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from pizza_ordering_service.models import Pizza, Order, Customer, Specification
-
+from pizza_ordering_service.validators import is_list, is_dict
 
 class PizzaSerializer(serializers.ModelSerializer):
     class Meta:
@@ -26,6 +26,7 @@ class SpecificationSerializer(serializers.ModelSerializer):
 
 class OrderSerializer(serializers.ModelSerializer):
     pizzas = SpecificationSerializer(source="specification_set", many=True, read_only=True)
+    customer = CustomerSerializer()
 
     class Meta:
         model = Order
@@ -33,34 +34,27 @@ class OrderSerializer(serializers.ModelSerializer):
         depth = 1
 
     def create(self, validated_data):
-        customer = Customer.objects.create(
-            zip_code = self.initial_data["customer"]["zip_code"],
-            city = self.initial_data["customer"]["city"],
-            name = self.initial_data["customer"]["name"],
-            address = self.initial_data["customer"]["address"]
-        )
+        customer_data = validated_data.pop("customer")
+        customer = Customer.objects.create(**customer_data)
         order = Order.objects.create(customer=customer, **validated_data)
+
         if "pizzas" in self.initial_data:
-            pizzas = self.initial_data.get("pizzas")
-            for pizza in pizzas:
-                size = pizza.get("size")
-                flavor = pizza.get("flavor")
-                quantity = pizza.get("quantity")
-                pizza_instance = Pizza.objects.get(flavor=flavor)
-                Specification(order=order, pizza=pizza_instance, size=size, quantity=quantity).save()
+            if is_list(self.initial_data.get("pizzas"), "pizzas"):
+                pizzas = self.initial_data.get("pizzas")
+
+                for pizza in pizzas:
+                    if is_dict(pizza, "pizza"):
+                        pizza_data = SpecificationSerializer(data=pizza)
+                        if pizza_data.is_valid():
+                            size = pizza.get("size")
+                            quantity = pizza.get("quantity")
+                            pizza_instance = Pizza.objects.get(flavor=pizza.get("flavor"))
+                            Specification(order=order, pizza=pizza_instance, size=size, quantity=quantity).save()
+
         order.save()
         return order
 
     def update(self, instance, validated_data):
-        Specification.objects.filter(Order=instance).delete()
-        pizzas = self.initial_data.get("pizzas")
-        for pizza in pizzas:
-            size = pizza.get("size")
-            flavor = pizza.get("flavor")
-            quantity = pizza.get("quantity")
-            pizza_instance = Pizza.objects.get(flavor=flavor)
-            Specification(order=order, pizza=pizza_instance, size=size, quantity=quantity).save()
-
-        instance.__dict__.update(**validated_data)
-        instance.save()
-        return instance
+        # Currently not updating but creating another object
+        Order.objects.filter(id=instance.id).delete()
+        return self.create(validated_data)
